e21b7921f074454a91351420de2f0612329a7777
sumalemambo <55157948+sumalemambo@users.noreply.github.com>
2020-08-16 20:35:46 -0400

    Update Problema 3.py

--- Certamen 1/2019/Problema 3.py
+++ Certamen 1/2019/Problema 3.py
@@ -1,9 +1,20 @@
+################################################
+# Funcion: lenght                              #
+# Input: un numero entero                      #
+# Descripcion: Obtiene el largo de un numero   #
+################################################
 def lenght(numero):
     num_size = 0
     while(numero != 0):
         numero = numero // 10
         num_size += 1
     return num_size
+
+################################################
+# Funcion: invert                              #
+# Input: un numero entero                      #
+# Descripcion: Invierte un numero dado         #
+################################################
 
 def invert(numero):
     num_size = lenght(numero)
@@ -13,6 +24,13 @@ def invert(numero):
         numero = numero // 10
         num_size -= 1
     return number
+
+###############################################################################
+# Funcion: Palidromico                                                        #
+# Input: un numero entero y un numero de pasos                                #
+# Descripcion: Verifica si un numero sumado a su inverso forman un palidromo, #
+# si falla trata de obetener un nuevo palidromo de esa suma                   #
+###############################################################################
 
 def palindromico(numero,pasos):
     numero += invert(numero)
